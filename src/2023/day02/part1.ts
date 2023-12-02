2aae0445e98009e09d75f86d163558ad88a8869b
Matthew Mincher <matthew@mincher.org>
2023-12-02 15:27:01 +0000

    Day 2

--- src/2023/day02/part1.ts
+++ src/2023/day02/part1.ts
@@ -1,5 +1,81 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+type CubeColour = "red" | "green" | "blue";
+type CubeExposure = {
+  colour: CubeColour;
+  count: number;
+};
+type GameRound = {
+  cubes: CubeExposure[];
+};
+type GameRules = {
+  cubeCounts: Record<CubeColour, number>;
+};
+type Game = {
+  id: number;
+  rounds: GameRound[];
+};
 
-// TODO: Complete Part 1
+const input = parseInput<Game>({
+  split: {
+    mapper: function (line) {
+      const gameId = line.match(/Game (\d+):/)?.[1];
+      const roundStrings = line.matchAll(
+        /([:|;] (?<a>\d+ [a-z]+)(, (?<b>\d+ [a-z]+))?(, (?<c>\d+ [a-z]+))?)/g
+      );
+
+      let rounds = [];
+
+      for (const roundString of roundStrings) {
+        let cubes: CubeExposure[] = [];
+
+        ["a", "b", "c"].forEach((groupName) => {
+          if (roundString.groups![groupName]) {
+            const split = roundString.groups![groupName].split(" ");
+
+            cubes.push({
+              colour: split[1] as CubeColour,
+              count: Number(split[0]),
+            });
+          }
+        });
+
+        rounds.push({
+          cubes: cubes,
+        });
+      }
+
+      return {
+        id: Number(gameId),
+        rounds: rounds,
+      };
+    },
+  },
+});
+
+const gameRules: GameRules = {
+  cubeCounts: {
+    red: 12,
+    green: 13,
+    blue: 14,
+  },
+};
+
+function gameIsPossible(game: Game): boolean {
+  return game.rounds.filter(roundIsPossible).length === game.rounds.length;
+}
+function roundIsPossible(round: GameRound): boolean {
+  return (
+    round.cubes.filter(
+      (exposure) => exposure.count <= gameRules.cubeCounts[exposure.colour]
+    ).length === round.cubes.length
+  );
+}
+
+function main() {
+  return input.filter(gameIsPossible).reduce((sum, game) => {
+    return sum + game.id;
+  }, 0);
+}
+
+export default main();
--- src/2023/day02/part2.ts
+++ src/2023/day02/part2.ts
@@ -1,5 +1,88 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const cubeColours = ["red", "green", "blue"] as const;
+type CubeColour = (typeof cubeColours)[number];
 
-// TODO: Complete Part 2
+type CubeExposure = {
+  colour: CubeColour;
+  count: number;
+};
+type GameRound = {
+  cubes: CubeExposure[];
+};
+type Game = {
+  id: number;
+  rounds: GameRound[];
+};
+
+const input = parseInput<Game>({
+  split: {
+    mapper: function (line) {
+      const gameId = line.match(/Game (\d+):/)?.[1];
+      const roundStrings = line.matchAll(
+        /([:|;] (?<a>\d+ [a-z]+)(, (?<b>\d+ [a-z]+))?(, (?<c>\d+ [a-z]+))?)/g
+      );
+
+      let rounds = [];
+
+      for (const roundString of roundStrings) {
+        let cubes: CubeExposure[] = [];
+
+        ["a", "b", "c"].forEach((groupName) => {
+          if (roundString.groups![groupName]) {
+            const split = roundString.groups![groupName].split(" ");
+
+            cubes.push({
+              colour: split[1] as CubeColour,
+              count: Number(split[0]),
+            });
+          }
+        });
+
+        rounds.push({
+          cubes: cubes,
+        });
+      }
+
+      return {
+        id: Number(gameId),
+        rounds: rounds,
+      };
+    },
+  },
+});
+
+type MinimumSet = Record<CubeColour, number>;
+const emptyMimimumSet: MinimumSet = {
+  red: 0,
+  green: 0,
+  blue: 0,
+};
+
+function findMinimumSetInGame(game: Game): MinimumSet {
+  return game.rounds.reduce((highCount: MinimumSet, round) => {
+    const roundMinimumSet = findMinimumSetInRound(round);
+
+    return {
+      red: Math.max(highCount.red, roundMinimumSet.red),
+      blue: Math.max(highCount.blue, roundMinimumSet.blue),
+      green: Math.max(highCount.green, roundMinimumSet.green),
+    };
+  }, emptyMimimumSet);
+}
+function findMinimumSetInRound(round: GameRound): MinimumSet {
+  return round.cubes.reduce((highCount: MinimumSet, exposure) => {
+    return {
+      ...highCount,
+      [exposure.colour]: Math.max(highCount[exposure.colour], exposure.count),
+    };
+  }, emptyMimimumSet);
+}
+
+function main() {
+  return input.map(findMinimumSetInGame).reduce((power, minimumSet) => {
+    return power + minimumSet.red * minimumSet.green * minimumSet.blue;
+  }, 0);
+}
+
+export default main();
