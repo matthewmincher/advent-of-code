c0d629b9a6e324f13e7796dc7f44843ec441a672
Matthew Mincher <matthew@mincher.org>
2023-12-03 12:45:08 +0000

    Day 3

--- src/2023/day03/part1.ts
+++ src/2023/day03/part1.ts
@@ -1,5 +1,87 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+class Coordinate {
+  row: number;
+  col: number;
 
-// TODO: Complete Part 1
+  constructor(row: number, col: number) {
+    this.row = row;
+    this.col = col;
+  }
+
+  neighbours(row: number, col: number): boolean {
+    for (
+      let y = Math.max(this.row - 1, 0);
+      y <= Math.min(this.row + 1, gridSize.rows);
+      y++
+    ) {
+      for (
+        let x = Math.max(this.col - 1, 0);
+        x <= Math.min(this.col + 1, gridSize.cols);
+        x++
+      ) {
+        if (y === row && x === col) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
+
+const gridSize = {
+  rows: input.length,
+  cols: input[0].length,
+};
+
+function main() {
+  let symbolCoordinates: Coordinate[] = [];
+
+  input.forEach((line, rowIndex) => {
+    for (const symbol of line.matchAll(/([^0-9.\n])/g)) {
+      symbolCoordinates.push(new Coordinate(rowIndex, symbol.index!));
+    }
+  });
+
+  let adjacentIndexPaths: string[] = [];
+  for (let y = 0; y <= gridSize.rows; y++) {
+    for (let x = 0; x <= gridSize.cols; x++) {
+      for (const coordinate of symbolCoordinates) {
+        if (coordinate.neighbours(y, x)) {
+          adjacentIndexPaths.push(`${y}.${x}`);
+          break;
+        }
+      }
+    }
+  }
+
+  return input.reduce((sum, line, rowIndex) => {
+    for (const matchedNumber of line.matchAll(/(\d+)/g)) {
+      let number = matchedNumber[0];
+
+      for (
+        let characterIndex = 0;
+        characterIndex < number.length;
+        characterIndex++
+      ) {
+        let path = `${rowIndex}.${matchedNumber.index! + characterIndex}`;
+
+        if (adjacentIndexPaths.indexOf(path) !== -1) {
+          sum += Number(number);
+
+          break;
+        }
+      }
+    }
+    return sum;
+  }, 0);
+}
+
+export default main();
--- src/2023/day03/part2.ts
+++ src/2023/day03/part2.ts
@@ -1,5 +1,104 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+class Coordinate {
+  row: number;
+  col: number;
 
-// TODO: Complete Part 2
+  constructor(row: number, col: number) {
+    this.row = row;
+    this.col = col;
+  }
+
+  neighbours(row: number, col: number): boolean {
+    for (
+      let y = Math.max(this.row - 1, 0);
+      y <= Math.min(this.row + 1, gridSize.rows);
+      y++
+    ) {
+      for (
+        let x = Math.max(this.col - 1, 0);
+        x <= Math.min(this.col + 1, gridSize.cols);
+        x++
+      ) {
+        if (y === row && x === col) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+class CoordinateGroup {
+  value: number;
+  members: Coordinate[];
+
+  constructor(value: number, members: Coordinate[]) {
+    this.value = value;
+    this.members = members;
+  }
+
+  neighbors(row: number, col: number): boolean {
+    return (
+      this.members.filter((coordinate) => coordinate.neighbours(row, col))
+        .length !== 0
+    );
+  }
+}
+
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
+
+const gridSize = {
+  rows: input.length,
+  cols: input[0].length,
+};
+
+function main() {
+  let partNumberGroups: CoordinateGroup[] = [];
+
+  input.forEach((line, rowIndex) => {
+    for (const partNumber of line.matchAll(/([0-9]+)/g)) {
+      let members: Coordinate[] = [];
+      let value = partNumber[0];
+
+      for (
+        let characterIndex = 0;
+        characterIndex < value.length;
+        characterIndex++
+      ) {
+        members.push(
+          new Coordinate(rowIndex, partNumber.index! + characterIndex)
+        );
+      }
+
+      partNumberGroups.push(
+        new CoordinateGroup(Number(partNumber[0]), members)
+      );
+    }
+  });
+
+  return input.reduce((sum, line, rowIndex) => {
+    for (const matchedGear of line.matchAll(/(\*)/g)) {
+      let neighboringGroups = partNumberGroups.filter((group) =>
+        group.neighbors(rowIndex, matchedGear.index!)
+      );
+
+      if (neighboringGroups.length < 2) {
+        continue;
+      }
+
+      sum += neighboringGroups.reduce((ratio, neighboringGroup) => {
+        return ratio * neighboringGroup.value;
+      }, 1);
+    }
+
+    return sum;
+  }, 0);
+}
+
+export default main();
