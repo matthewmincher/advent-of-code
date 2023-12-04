dd3e93ea584151f9e30ed6c59c7064545d288811
Matthew Mincher <matthew@mincher.org>
2023-12-04 19:36:35 +0000

    Day 4.2

--- src/2023/day04/part2.ts
+++ src/2023/day04/part2.ts
@@ -1,5 +1,63 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+class Card {
+  winningNumbers: number[];
+  visibleNumbers: number[];
 
-// TODO: Complete Part 2
+  additionalCards: Card[] = [];
+  count = 1;
+
+  constructor(winningNumbers: number[], visibleNumbers: number[]) {
+    this.winningNumbers = winningNumbers;
+    this.visibleNumbers = visibleNumbers;
+  }
+
+  matchedNumbers(): number[] {
+    return this.visibleNumbers.filter((number) =>
+      this.winningNumbers.includes(number)
+    );
+  }
+
+  copy(): Card {
+    return new Card(this.winningNumbers, this.visibleNumbers);
+  }
+}
+
+const originalCards = parseInput<Card>({
+  split: {
+    mapper: (line) => {
+      const winningNumbers = [];
+      const visibleNumbers = [];
+
+      line = line.substring(line.indexOf(":"));
+      const dividerIndex = line.indexOf("|");
+      const numbers = line.matchAll(/(\d+)/g);
+
+      for (const number of numbers) {
+        if (number.index! < dividerIndex) {
+          winningNumbers.push(Number(number[0]));
+        } else {
+          visibleNumbers.push(Number(number[0]));
+        }
+      }
+
+      return new Card(winningNumbers, visibleNumbers);
+    },
+  },
+});
+
+function main() {
+  const cardQuantities = originalCards.map(() => 1);
+
+  originalCards.forEach((card, index) => {
+    const matchedNumbers = card.matchedNumbers();
+
+    for (let i = 1; i <= matchedNumbers.length; i++) {
+      cardQuantities[index + i] += cardQuantities[index] ?? 1;
+    }
+  });
+
+  return cardQuantities.reduce((sum, quantity) => sum + quantity, 0);
+}
+
+export default main();
