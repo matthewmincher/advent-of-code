5af220f109936aed8f47c37cd4af799bd9e4ebd8
Matthew Mincher <matthew@mincher.org>
2023-12-04 13:41:33 +0000

    Day 4.1

--- src/2023/day04/part1.ts
+++ src/2023/day04/part1.ts
@@ -1,5 +1,46 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+type Card = {
+  winningNumbers: number[];
+  visibleNumbers: number[];
+};
 
-// TODO: Complete Part 1
+const input = parseInput<Card>({
+  split: {
+    mapper: (line: string) => {
+      const card: Card = {
+        winningNumbers: [],
+        visibleNumbers: [],
+      };
+
+      line = line.substring(line.indexOf(":"));
+      const dividerIndex = line.indexOf("|");
+      const numbers = line.matchAll(/(\d+)/g);
+
+      for (const number of numbers) {
+        if (number.index! < dividerIndex) {
+          card.winningNumbers.push(Number(number[0]));
+        } else {
+          card.visibleNumbers.push(Number(number[0]));
+        }
+      }
+
+      return card;
+    },
+  },
+});
+
+function main() {
+  return input
+    .map((card) => {
+      return card.visibleNumbers.filter((number) =>
+        card.winningNumbers.includes(number)
+      );
+    })
+    .filter((winningNumbers) => winningNumbers.length > 0)
+    .reduce((sum, winningNumbers) => {
+      return sum + 1 * Math.max(Math.pow(2, winningNumbers.length - 1), 1);
+    }, 0);
+}
+
+export default main();
