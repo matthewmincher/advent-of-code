a3465440a531b933b63c7832662b8ce5e51fbb99
Matthew Mincher <matthew@mincher.org>
2023-12-06 23:02:40 +0000

    Day 6

--- src/2023/day06/part1.ts
+++ src/2023/day06/part1.ts
@@ -1,5 +1,44 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 1
+type Race = {
+  duration: number;
+  recordDistance: number;
+};
+
+const times = input[0].match(/\d+/g);
+const distances = input[1].match(/\d+/g);
+
+const races: Race[] = times!.map((time, index) => {
+  return {
+    duration: Number(time),
+    recordDistance: Number(distances![index]),
+  };
+});
+
+function main() {
+  return races
+    .map((race) => {
+      const winningHolds: number[] = [];
+
+      for (let i = 0; i < race.duration; i++) {
+        const distance = i * (race.duration - i);
+
+        if (distance > race.recordDistance) {
+          winningHolds.push(i);
+        }
+      }
+
+      return winningHolds;
+    })
+    .reduce((sum, raceResults) => {
+      return sum * raceResults.length;
+    }, 1);
+}
+
+export default main();
--- src/2023/day06/part2.ts
+++ src/2023/day06/part2.ts
@@ -1,5 +1,27 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+const time = Number(input[0].match(/\d+/g)!.join(""));
+const recordDistance = Number(input[1].match(/\d+/g)!.join(""));
+
+function main() {
+  // There's 100% a maths answer here...
+  const winningHolds: number[] = [];
+
+  for (let i = 0; i < time; i++) {
+    const distance = i * (time - i);
+
+    if (distance > recordDistance) {
+      winningHolds.push(i);
+    }
+  }
+
+  return winningHolds.length;
+}
+
+export default main();
