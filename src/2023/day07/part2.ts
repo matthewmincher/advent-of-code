2e0c3339508be19bd2e75fd76d4a5ded38122bb4
Matthew Mincher <matthew.mincher@sykescottages.co.uk>
2023-12-07 14:16:18 +0000

    Day 7.2

--- src/2023/day07/part2.ts
+++ src/2023/day07/part2.ts
@@ -1,5 +1,125 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const distinctScoreConfiguration: Record<number, number> = {
+  1: 1,
+  2: 5,
+  3: 20,
+  4: 50,
+  5: 100,
+};
 
-// TODO: Complete Part 2
+const cardValue = [
+  "J",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "T",
+  "Q",
+  "K",
+  "A",
+];
+
+class Hand {
+  cards: string[];
+  bid: number;
+
+  sets: Record<string, number> = {};
+
+  constructor(cards: string[], bid: number) {
+    this.cards = cards;
+    this.bid = bid;
+
+    let jokerCount = 0;
+    this.cards.forEach((card) => {
+      if (card === "J") {
+        jokerCount++;
+      } else {
+        this.sets[card] = (this.sets[card] ?? 0) + 1;
+      }
+    });
+
+    if (jokerCount === 5) {
+      this.sets["J"] = 5;
+    } else {
+      let largestDistinctCard: string | null = null;
+      let largestDistinct = 0;
+      for (const [card, count] of Object.entries(this.sets)) {
+        if (count > largestDistinct) {
+          largestDistinct = count;
+          largestDistinctCard = card;
+        }
+      }
+
+      this.sets[largestDistinctCard!] += jokerCount;
+    }
+  }
+
+  distinctCardCount(): number {
+    return Object.keys(this.sets).length;
+  }
+
+  distinctCardScore(): number {
+    let score = 0;
+
+    for (const [key, value] of Object.entries(this.sets)) {
+      score += distinctScoreConfiguration[value];
+    }
+
+    return score;
+  }
+
+  compareTo(hand: Hand): number {
+    const aDistinctCount = this.distinctCardCount(),
+      bDistinctCount = hand.distinctCardCount();
+
+    if (aDistinctCount === bDistinctCount) {
+      const aDisctinctScore = this.distinctCardScore(),
+        bDistinctScore = hand.distinctCardScore();
+
+      if (aDisctinctScore === bDistinctScore) {
+        for (let i = 0; i < 5; i++) {
+          const aCard = this.cards[i],
+            bCard = hand.cards[i];
+
+          const aCardScore = cardValue.indexOf(aCard);
+          const bCardScore = cardValue.indexOf(bCard);
+
+          if (aCardScore != bCardScore) {
+            return bCardScore - aCardScore;
+          }
+        }
+      }
+
+      return bDistinctScore - aDisctinctScore;
+    }
+
+    return aDistinctCount - bDistinctCount;
+  }
+}
+
+const input = parseInput<Hand>({
+  split: {
+    mapper: (line): Hand => {
+      const [cards, bid] = line.split(" ");
+
+      return new Hand(cards.split(""), Number(bid));
+    },
+  },
+});
+
+function main() {
+  return input
+    .sort((a, b) => a.compareTo(b))
+    .reverse()
+    .reduce((sum, hand, index) => {
+      return sum + hand.bid * (index + 1);
+    }, 0);
+}
+
+export default main();
