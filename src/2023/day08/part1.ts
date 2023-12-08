e17017af78a2d5da1106e7407bf3082bdc5a3462
Matthew Mincher <matthew@mincher.org>
2023-12-08 19:27:33 +0000

    Day 8.1

--- src/2023/day08/part1.ts
+++ src/2023/day08/part1.ts
@@ -1,5 +1,58 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+}).filter(Boolean);
 
-// TODO: Complete Part 1
+type Direction = "L" | "R";
+
+type Node = {
+  name: string;
+  L: Direction;
+  R: Direction;
+};
+
+let instructions: Direction[];
+let nodes: Record<string, Node> = {};
+
+function getInstructionForStep(step: number): Direction {
+  const index = step % instructions.length;
+
+  return instructions[index]!;
+}
+
+input.forEach((line, index) => {
+  if (!index) {
+    instructions = line.split("") as Direction[];
+  } else {
+    const [_, name, L, R] = line.match(/^([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)$/)!;
+
+    nodes[name] = {
+      name,
+      L: L as Direction,
+      R: R as Direction,
+    };
+  }
+});
+
+function main() {
+  let position = "AAA";
+
+  let step = 0;
+  while (true) {
+    const currentNode = nodes[position]!;
+    const direction = getInstructionForStep(step);
+    const nextNodeName = currentNode[direction];
+
+    position = nextNodeName;
+    step++;
+
+    if (position === "ZZZ") {
+      return step;
+    }
+  }
+}
+
+export default main();
