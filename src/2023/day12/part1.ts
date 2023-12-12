b37420ab825594a309bd3a13173c0525ff33da94
Matthew Mincher <matthew@mincher.org>
2023-12-12 21:31:30 +0000

    Day 12

--- src/2023/day12/part1.ts
+++ src/2023/day12/part1.ts
@@ -1,5 +1,63 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+type OperationalSpring = ".";
+type BrokenSpring = "#";
+type UnknownSpring = "?";
 
-// TODO: Complete Part 1
+type Spring = OperationalSpring | BrokenSpring | UnknownSpring;
+
+type Row = {
+  springs: Spring[];
+  groups: number[];
+};
+
+const input = parseInput<Row>({
+  split: {
+    mapper: (line) => {
+      const [chars, groups] = line.split(" ");
+
+      return {
+        springs: chars.split("") as Spring[],
+        groups: groups.split(",").map(Number),
+      };
+    },
+  },
+});
+
+function getCombinationCount(springs: Spring[], groups: number[]): number {
+  if (!springs.length) {
+    return groups.length ? 0 : 1; // No springs left but we should have
+  }
+
+  if (!groups.length) {
+    return springs.includes("#") ? 0 : 1; // No broken left, but we should have
+  }
+
+  const spring = springs[0];
+  const group = groups[0];
+
+  let count = 0;
+
+  if (spring === "." || spring === "?") {
+    count += getCombinationCount(springs.slice(1), groups);
+  }
+
+  if (spring === "#" || spring === "?") {
+    if (
+      group <= springs.length && // Enough springs left?
+      !springs.slice(0, group).includes(".") && // No working springs in the group
+      (group === springs.length || springs[group] !== "#") // No springs after group or trailing spring isn't broken
+    ) {
+      count += getCombinationCount(springs.slice(group + 1), groups.slice(1));
+    }
+  }
+  return count;
+}
+
+function main() {
+  return input
+    .map((row) => getCombinationCount(row.springs, row.groups))
+    .reduce((last, next) => last + next);
+}
+
+export default main();
--- src/2023/day12/part2.ts
+++ src/2023/day12/part2.ts
@@ -1,5 +1,87 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+type OperationalSpring = ".";
+type BrokenSpring = "#";
+type UnknownSpring = "?";
 
-// TODO: Complete Part 2
+type Spring = OperationalSpring | BrokenSpring | UnknownSpring;
+
+type Row = {
+  springs: Spring[];
+  groups: number[];
+};
+
+const input = parseInput<Row>({
+  split: {
+    mapper: (line) => {
+      const [chars, groups] = line.split(" ");
+
+      return {
+        springs: chars.split("") as Spring[],
+        groups: groups.split(",").map(Number),
+      };
+    },
+  },
+});
+
+const resultCache: Map<string, number> = new Map();
+
+function getCombinationCount(springs: Spring[], groups: number[]): number {
+  const cacheKey = JSON.stringify({
+    springs,
+    groups,
+  });
+  if (resultCache.has(cacheKey)) {
+    return resultCache.get(cacheKey)!;
+  }
+
+  if (!springs.length) {
+    return groups.length ? 0 : 1;
+  }
+
+  if (!groups.length) {
+    return springs.includes("#") ? 0 : 1;
+  }
+
+  const spring = springs[0];
+  const group = groups[0];
+
+  let count = 0;
+
+  if (spring === "." || spring === "?") {
+    count += getCombinationCount(springs.slice(1), groups);
+  }
+
+  if (spring === "#" || spring === "?") {
+    if (
+      group <= springs.length &&
+      !springs.slice(0, group).includes(".") &&
+      (group === springs.length || springs[group] !== "#")
+    ) {
+      count += getCombinationCount(springs.slice(group + 1), groups.slice(1));
+    }
+  }
+
+  resultCache.set(cacheKey, count);
+
+  return count;
+}
+
+function expandRow(row: Row): Row {
+  return {
+    springs: Array.from(new Array(5))
+      .fill(row.springs.join(""))
+      .join("?")
+      .split("") as Spring[],
+    groups: Array.from(new Array(5).fill(row.groups)).flat(),
+  };
+}
+
+function main() {
+  return input
+    .map(expandRow)
+    .map((row) => getCombinationCount(row.springs, row.groups))
+    .reduce((last, next) => last + next);
+}
+
+export default main();
