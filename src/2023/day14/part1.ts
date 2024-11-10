45e09ce4c30517100d5a9d4cf1ed4d9d336bc30b
Matthew Mincher <matthew@mincher.org>
2024-11-10 19:47:40 +0000

    Day 14 part 1

--- src/2023/day14/part1.ts
+++ src/2023/day14/part1.ts
@@ -1,5 +1,53 @@
 import { parseInput } from '../../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+})
+  .split('\n')
+  .map((line) => line.split(''));
 
-// TODO: Complete Part 1
+function slideBouldersUp(board: string[][]) {
+  const columns = board[0].length;
+
+  for (let x = 0; x < columns; x++) {
+    let stackYPos = 0;
+
+    for (let y = 0; y < board.length; y++) {
+      const char = board[y][x];
+
+      if (char === '#') {
+        stackYPos = y + 1;
+
+        continue;
+      }
+
+      if (char === 'O') {
+        if (stackYPos !== y) {
+          board[stackYPos][x] = char;
+          board[y][x] = '.';
+        }
+
+        stackYPos++;
+      }
+    }
+  }
+}
+
+function main() {
+  slideBouldersUp(input);
+  const rowCount = input.length;
+  return input
+    .map((row, rowIndex) => {
+      return (
+        row
+          .map((char) => (char === 'O' ? 1 : 0))
+          .reduce((previous, current) => previous + current, 0 as number) *
+        (rowCount - rowIndex)
+      );
+    })
+    .reduce((previous, current) => {
+      return previous + current;
+    }, 0);
+}
+
+export default main();
