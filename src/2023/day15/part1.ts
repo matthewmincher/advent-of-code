d89a5b30d7be5a2d22452fb4f5daedab90d9dd9a
Matthew Mincher <matthew@mincher.org>
2024-11-10 20:46:00 +0000

    Day 15

--- src/2023/day15/part1.ts
+++ src/2023/day15/part1.ts
@@ -1,5 +1,27 @@
 import { parseInput } from '../../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+})
+  .replace(/\n/g, '')
+  .split(',');
 
-// TODO: Complete Part 1
+const main = () => {
+  return input
+    .map((string) => {
+      let val = 0;
+
+      for (const char of string) {
+        val += char.charCodeAt(0);
+        val *= 17;
+        val = val % 256;
+      }
+
+      return val;
+    })
+    .reduce((sum, val) => {
+      return sum + val;
+    }, 0);
+};
+
+export default main();
--- src/2023/day15/part2.ts
+++ src/2023/day15/part2.ts
@@ -1,5 +1,69 @@
 import { parseInput } from '../../util';
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+})
+  .replace(/\n/g, '')
+  .split(',');
 
-// TODO: Complete Part 2
+const getHashValue = (string: string) => {
+  let val = 0;
+
+  for (const char of string) {
+    val += char.charCodeAt(0);
+    val *= 17;
+    val = val % 256;
+  }
+
+  return val;
+};
+
+type Lens = {
+  label: string;
+  focalLength: string;
+};
+
+const main = () => {
+  const boxes: Lens[][] = Array(256)
+    .fill(0)
+    .map(() => []);
+
+  for (const instruction of input) {
+    const [_, label, operation, focalLength] = instruction.match(
+      /([a-z]+)([-=])([0-9]+)?/,
+    )!;
+    const boxIndex = getHashValue(label);
+    const box = boxes[boxIndex];
+
+    if (operation === '-') {
+      boxes[boxIndex] = box.filter((lens) => lens.label !== label);
+    } else if (operation === '=') {
+      const indexOfLensWithLabel = boxes[boxIndex].findIndex(
+        (lens) => lens.label === label,
+      );
+      const lens = {
+        label,
+        focalLength,
+      };
+
+      if (indexOfLensWithLabel !== -1) {
+        boxes[boxIndex][indexOfLensWithLabel] = lens;
+      } else {
+        boxes[boxIndex].push(lens);
+      }
+    }
+  }
+
+  return boxes.reduce((sum, box, boxIndex) => {
+    return (
+      sum +
+      box.reduce((power, lens, lensIndex) => {
+        return (
+          power + (boxIndex + 1) * (lensIndex + 1) * Number(lens.focalLength)
+        );
+      }, 0)
+    );
+  }, 0);
+};
+
+export default main();
