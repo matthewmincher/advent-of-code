e7e15b6613fc11e571dea6f2e9bd75118cc232dc
Matthew Mincher <matthew@mincher.org>
2024-12-01 13:56:09 +0000

    2024.1

--- src/2024/day01/part1.ts
+++ src/2024/day01/part1.ts
@@ -1,5 +1,35 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 1
+function main() {
+  const columns = input.reduce(
+    (cols, line) => {
+      const numbers = line.split(/\s+/);
+
+      cols[0].push(Number(numbers[0]));
+      cols[1].push(Number(numbers[1]));
+
+      return cols;
+    },
+    [[], []] as number[][],
+  );
+
+  for (const i in columns) {
+    columns[i].sort();
+  }
+
+  const count = columns[0].length;
+  let diff = 0;
+  for (let i = 0; i < count; i++) {
+    diff += Math.abs(columns[0][i] - columns[1][i]);
+  }
+
+  return diff;
+}
+
+export default main();
--- src/2024/day01/part2.ts
+++ src/2024/day01/part2.ts
@@ -1,5 +1,36 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+function main() {
+  const columns = input.reduce(
+    (cols, line) => {
+      const numbers = line.split(/\s+/);
+
+      cols[0].push(Number(numbers[0]));
+      cols[1].push(Number(numbers[1]));
+
+      return cols;
+    },
+    [[], []] as number[][],
+  );
+
+  for (const i in columns) {
+    columns[i].sort();
+  }
+
+  const count = columns[0].length;
+  let similarity = 0;
+  for (let i = 0; i < count; i++) {
+    const instances = columns[1].filter((n) => n == columns[0][i]).length;
+    similarity += columns[0][i] * instances;
+  }
+
+  return similarity;
+}
+
+export default main();
