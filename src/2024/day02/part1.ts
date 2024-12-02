2b0ec39ee90534953d8a742098ce3ca0eea90554
Matthew Mincher <matthew@mincher.org>
2024-12-02 09:36:29 +0000

    2024.2

--- src/2024/day02/part1.ts
+++ src/2024/day02/part1.ts
@@ -1,5 +1,50 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 1
+const isSorted = (array: number[]) => {
+  return isAscending(array) || isDescending(array);
+};
+
+const isAscending = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+const isDescending = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < array[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isSafeChange = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    const delta = Math.abs(array[i] - array[i + 1]);
+    if (delta < 1 || delta > 3) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function main() {
+  return input
+    .map((line) => line.split(/\s+/).map((s) => Number(s)))
+    .filter(isSorted)
+    .filter(isSafeChange).length;
+}
+
+export default main();
--- src/2024/day02/part2.ts
+++ src/2024/day02/part2.ts
@@ -1,5 +1,61 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<string>({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+const isSorted = (array: number[]) => {
+  return isAscending(array) || isDescending(array);
+};
+
+const isAscending = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isDescending = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < array[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isSafeChange = (array: number[]) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    const delta = Math.abs(array[i] - array[i + 1]);
+    if (delta < 1 || delta > 3) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function main() {
+  return input
+    .map((line) => line.split(/\s+/).map((s) => Number(s)))
+    .map((values) => [
+      values,
+      ...values.map((value, index) => values.toSpliced(index, 1)),
+    ])
+    .filter((combinations) => {
+      for (const combination of combinations) {
+        if (isSorted(combination) && isSafeChange(combination)) {
+          return true;
+        }
+      }
+      return false;
+    }).length;
+}
+
+export default main();
