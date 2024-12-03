fcde8aa6aec3520d9b5b472c1236d1d89bf3f261
Matthew Mincher <matthew@mincher.org>
2024-12-03 08:36:49 +0000

    2024.3

--- src/2024/day03/part1.ts
+++ src/2024/day03/part1.ts
@@ -1,5 +1,22 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+});
 
-// TODO: Complete Part 1
+function main() {
+  const ops = input.matchAll(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+
+  let sum = 0;
+  for (const op of ops) {
+    const parts = op[0].split(",");
+    const first = parts[0].replace("mul(", "");
+    const second = parts[1].replace(")", "");
+
+    sum += Number(first) * Number(second);
+  }
+
+  return sum;
+}
+
+export default main();
--- src/2024/day03/part2.ts
+++ src/2024/day03/part2.ts
@@ -1,5 +1,39 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+});
 
-// TODO: Complete Part 2
+function main() {
+  const ops = input.matchAll(
+    /(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\)|don't\(\))/g,
+  );
+
+  let sum = 0;
+  let isOn = true;
+  for (const op of ops) {
+    if (op[0] === "do()") {
+      isOn = true;
+      continue;
+    }
+
+    if (op[0] === "don't()") {
+      isOn = false;
+      continue;
+    }
+
+    if (!isOn) {
+      continue;
+    }
+
+    const parts = op[0].split(",");
+    const first = parts[0].replace("mul(", "");
+    const second = parts[1].replace(")", "");
+
+    sum += Number(first) * Number(second);
+  }
+
+  return sum;
+}
+
+export default main();
