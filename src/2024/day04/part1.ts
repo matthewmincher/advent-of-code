e1b2a5df7eff41067a20f57481f5546dca5ce382
Matthew Mincher <matthew@mincher.org>
2024-12-04 07:07:25 +0000

    2024.4

--- src/2024/day04/part1.ts
+++ src/2024/day04/part1.ts
@@ -1,5 +1,59 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 1
+const sequence = ["X", "M", "A", "S"];
+
+const hasSequence = (
+  letters: string[][],
+  row: number,
+  col: number,
+  nextRow: number,
+  nextCol: number,
+): boolean => {
+  for (let i = 0; i < sequence.length; i++) {
+    const targetRow = row + i * nextRow;
+    const targetCol = col + i * nextCol;
+
+    if (
+      targetRow < 0 ||
+      targetCol < 0 ||
+      targetRow >= letters.length ||
+      targetCol >= letters[0].length
+    ) {
+      return false;
+    }
+
+    if (letters[targetRow][targetCol] !== sequence[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function main() {
+  const letters = input.map((line) => line.split(""));
+
+  let countXmas = 0;
+  for (let row = 0; row < letters.length; row++) {
+    for (let col = 0; col < letters[0].length; col++) {
+      for (let nextRowDir = -1; nextRowDir <= 1; nextRowDir++) {
+        for (let nextColDir = -1; nextColDir <= 1; nextColDir++) {
+          if (hasSequence(letters, row, col, nextRowDir, nextColDir)) {
+            // hadouken
+            countXmas++;
+          }
+        }
+      }
+    }
+  }
+
+  return countXmas;
+}
+
+export default main();
--- src/2024/day04/part2.ts
+++ src/2024/day04/part2.ts
@@ -1,5 +1,47 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+const hasShape = (letters: string[][], row: number, col: number): boolean => {
+  if (letters[row][col] !== "A") {
+    return false;
+  }
+
+  if (row - 1 < 0 || col - 1 < 0) {
+    return false;
+  }
+
+  if (row + 1 >= letters.length || col + 1 > letters[0].length) {
+    return false;
+  }
+
+  const topLeft = letters[row - 1][col - 1];
+  const bottomLeft = letters[row + 1][col - 1];
+  const bottomRight = letters[row + 1][col + 1];
+  const topRight = letters[row - 1][col + 1];
+
+  return ["MSMS", "MMSS", "SMSM", "SSMM"].includes(
+    `${topLeft}${topRight}${bottomLeft}${bottomRight}`,
+  );
+};
+
+function main() {
+  const letters = input.map((line) => line.split(""));
+
+  let countXmas = 0;
+  for (let row = 0; row < letters.length; row++) {
+    for (let col = 0; col < letters[0].length; col++) {
+      if (hasShape(letters, row, col)) {
+        countXmas++;
+      }
+    }
+  }
+
+  return countXmas;
+}
+
+export default main();
