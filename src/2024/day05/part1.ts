b80022965a2a94a7b7d19bdbd58f661025a90f44
Matthew Mincher <matthew@mincher.org>
2024-12-05 08:32:49 +0000

    2024.5

--- src/2024/day05/part1.ts
+++ src/2024/day05/part1.ts
@@ -1,5 +1,38 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+});
 
-// TODO: Complete Part 1
+function main() {
+  const [rulesString, pagesString] = input.split("\n\n");
+
+  const rules = rulesString.split("\n").map((rule) => rule.split("|"));
+  const pageList = pagesString.split("\n").map((pages) => pages.split(","));
+
+  return pageList
+    .filter((pages) => {
+      for (const numbers of rules) {
+        const [first, second] = numbers;
+        const filtered = pages.filter((page) => [first, second].includes(page));
+
+        if (!filtered.includes(first) || !filtered.includes(second)) {
+          continue;
+        }
+
+        if (!filtered.join().includes(`${first},${second}`)) {
+          return false;
+        }
+      }
+
+      return true;
+    })
+    .map((valid) => {
+      return valid[Math.floor(valid.length / 2)];
+    })
+    .reduce((acc, val) => {
+      return acc + Number(val);
+    }, 0);
+}
+
+export default main();
--- src/2024/day05/part2.ts
+++ src/2024/day05/part2.ts
@@ -1,5 +1,54 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: false,
+});
 
-// TODO: Complete Part 2
+function main() {
+  const [rulesString, pagesString] = input.split("\n\n");
+
+  const rules = rulesString.split("\n").map((rule) => rule.split("|"));
+  const pageList = pagesString.split("\n").map((pages) => pages.split(","));
+
+  return pageList
+    .filter((pages) => {
+      for (const [first, second] of rules) {
+        const filtered = pages.filter((page) => [first, second].includes(page));
+
+        if (!filtered.includes(first) || !filtered.includes(second)) {
+          continue;
+        }
+
+        if (!filtered.join().includes(`${first},${second}`)) {
+          return true;
+        }
+      }
+
+      return false;
+    })
+    .map((invalid) => {
+      const working = [...invalid];
+
+      working.sort((a, b) => {
+        for (const rule of rules) {
+          if (!rule.includes(a) || !rule.includes(b)) {
+            continue;
+          }
+
+          return rule.indexOf(a) - rule.indexOf(b);
+        }
+
+        return 0;
+      });
+
+      return working;
+    })
+    .map((valid) => {
+      return valid[Math.floor(valid.length / 2)];
+    })
+    .reduce((acc, val) => {
+      return acc + Number(val);
+    }, 0);
+}
+
+export default main();
