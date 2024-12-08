a4beb3c168514c5c51fc27be9f5938bdf74c185d
Matthew Mincher <matthew@mincher.org>
2024-12-08 13:36:18 +0000

    2024.8

--- src/2024/day08/part2.ts
+++ src/2024/day08/part2.ts
@@ -1,5 +1,91 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
+import { domainToASCII } from "node:url";
 
-const input = parseInput();
+const input = parseInput<string[]>({
+  split: {
+    delimiter: "\n",
+    mapper: (e) => e.split(""),
+  },
+});
 
-// TODO: Complete Part 2
+const EMPTY_SPACE = ".";
+
+const findPossibleLocations = (
+  targetChar: string,
+  targetRow: number,
+  targetCol: number,
+) => {
+  const possibilities: [row: number, col: number][] = [];
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      const char = input[row][col];
+
+      if (char !== targetChar) {
+        continue;
+      }
+
+      if (targetRow === row && targetCol === col) {
+        continue;
+      }
+
+      const delta = [targetRow - row, targetCol - col];
+
+      let rowNow = row + delta[0];
+      let colNow = col + delta[1];
+
+      while (isInBounds(rowNow, colNow)) {
+        possibilities.push([rowNow, colNow]);
+
+        rowNow += delta[0];
+        colNow += delta[1];
+      }
+    }
+  }
+
+  return possibilities;
+};
+
+const isInBounds = (row: number, col: number) => {
+  if (row < 0 || col < 0) {
+    return false;
+  }
+
+  if (row >= input.length || col >= input[0].length) {
+    return false;
+  }
+
+  return true;
+};
+
+function main() {
+  const antinodes = new Set<string>();
+  const dirtyMap = JSON.parse(JSON.stringify(input)) as string[][];
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[0].length; col++) {
+      const char = input[row][col];
+
+      if (char === EMPTY_SPACE) {
+        continue;
+      }
+
+      const locations = findPossibleLocations(char, row, col).filter(([r, c]) =>
+        isInBounds(r, c),
+      );
+
+      for (const [y, x] of locations) {
+        dirtyMap[y][x] = "#";
+        antinodes.add(`${y}-${x}`);
+      }
+    }
+  }
+
+  for (const row of dirtyMap) {
+    console.log(row.join(","));
+  }
+
+  return antinodes.size;
+}
+
+export default main();
