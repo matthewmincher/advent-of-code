3d4f94698ed826afc4c9754e04095accfbdb2b74
Matthew Mincher <matthew@mincher.org>
2024-12-08 00:48:55 +0000

    2024.7

--- src/2024/day07/part2.ts
+++ src/2024/day07/part2.ts
@@ -1,5 +1,72 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput<Calibration>({
+  split: {
+    delimiter: "\n",
+    mapper: (e) => {
+      const [value, numbers] = e.split(":");
 
-// TODO: Complete Part 2
+      return {
+        value: Number(value),
+        numbers: numbers
+          .trim()
+          .split(" ")
+          .map((s) => Number(s)),
+      };
+    },
+  },
+});
+
+enum Operation {
+  ADD,
+  MULTIPLY,
+}
+
+const tryValid = (
+  target: number,
+  value: number,
+  remainingInput: number[],
+): boolean => {
+  if (remainingInput.length === 0) {
+    return value === target;
+  }
+
+  if (value > target) {
+    return false;
+  }
+
+  if (tryValid(target, value + remainingInput[0], remainingInput.slice(1))) {
+    return true;
+  }
+
+  if (tryValid(target, value * remainingInput[0], remainingInput.slice(1))) {
+    return true;
+  }
+
+  return tryValid(
+    target,
+    Number(`${value}${remainingInput[0]}`),
+    remainingInput.slice(1),
+  );
+};
+
+type Calibration = {
+  value: number;
+  numbers: number[];
+};
+
+function main() {
+  return input
+    .map((calibration) => {
+      return tryValid(
+        calibration.value,
+        calibration.numbers[0],
+        calibration.numbers.slice(1),
+      )
+        ? calibration.value
+        : 0;
+    })
+    .reduce((acc, val) => acc + val, 0);
+}
+
+export default main();
