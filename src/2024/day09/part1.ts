8cb1143b68511dedac41c6c86ef3ec74949cf94a
Matthew Mincher <matthew@mincher.org>
2024-12-09 18:06:43 +0000

    2024.9

--- src/2024/day09/part1.ts
+++ src/2024/day09/part1.ts
@@ -1,5 +1,46 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: { delimiter: "" },
+});
 
-// TODO: Complete Part 1
+const EMPTY_SPACE = ".";
+
+function main() {
+  let isFreeSpace = false;
+  let fileId = 0;
+
+  const els = input.flatMap((number) => {
+    const arr = new Array<string>(number);
+
+    arr.fill(isFreeSpace ? "." : `${fileId++}`);
+
+    isFreeSpace = !isFreeSpace;
+
+    return arr;
+  });
+
+  for (let i = els.length - 1; i >= 0; i--) {
+    if (els[i] === EMPTY_SPACE) {
+      continue;
+    }
+
+    const placeAt = els.findIndex((c, j) => c === EMPTY_SPACE && j < i);
+    if (placeAt === -1) {
+      continue;
+    }
+
+    els[placeAt] = els[i];
+    els[i] = EMPTY_SPACE;
+  }
+
+  return els.reduce((acc, char, index) => {
+    if (char === EMPTY_SPACE) {
+      return acc;
+    }
+
+    return acc + index * Number(char);
+  }, 0);
+}
+
+export default main();
--- src/2024/day09/part2.ts
+++ src/2024/day09/part2.ts
@@ -1,5 +1,66 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: { delimiter: "" },
+});
 
-// TODO: Complete Part 2
+const EMPTY_SPACE = ".";
+
+function main() {
+  let isFreeSpace = false;
+  let fileId = 0;
+
+  const els = input.flatMap((number) => {
+    const arr = new Array<string>(number);
+
+    arr.fill(isFreeSpace ? "." : `${fileId++}`);
+
+    isFreeSpace = !isFreeSpace;
+
+    return arr;
+  });
+
+  const fileSizes = new Map<string, number>();
+
+  for (const el of els) {
+    if (el === EMPTY_SPACE) {
+      continue;
+    }
+
+    fileSizes.set(el, (fileSizes.get(el) ?? 0) + 1);
+  }
+
+  for (let i = els.length - 1; i >= 0; i--) {
+    const file = els[i];
+
+    if (file === EMPTY_SPACE) {
+      continue;
+    }
+
+    const size = fileSizes.get(file)!;
+
+    const placeAt = els.findIndex(
+      (c, j) =>
+        c === EMPTY_SPACE && // Start at empty space
+        j < i && // Don't move right
+        els.slice(j, j + size).every((c) => c === EMPTY_SPACE), // There's enough trailing space
+    );
+
+    if (placeAt === -1) {
+      continue;
+    }
+
+    els.fill(file, placeAt, placeAt + size); // Place file into free space
+    els.fill(EMPTY_SPACE, i - size + 1, i + 1); // Replace the file with free space (backwards as we're at the end of a file)
+  }
+
+  return els.reduce((acc, char, index) => {
+    if (char === EMPTY_SPACE) {
+      return acc;
+    }
+
+    return acc + index * Number(char);
+  }, 0);
+}
+
+export default main();
