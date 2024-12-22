572c68ad53d84e74056c7aae7f14afcfedbab2f9
Matthew Mincher <matthew@mincher.org>
2024-12-22 17:46:55 +0000

    2024.11

--- src/2024/day11/part1.ts
+++ src/2024/day11/part1.ts
@@ -1,5 +1,33 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: " ",
+  },
+});
 
-// TODO: Complete Part 1
+const blinkAtStone = (number: number): number[] => {
+  if (number === 0) {
+    return [1];
+  }
+
+  const numbers = `${number}`;
+  if (numbers.length % 2 === 0) {
+    const mid = Math.ceil(numbers.length / 2);
+    return [Number(numbers.slice(0, mid)), Number(numbers.slice(mid))];
+  }
+
+  return [number * 2024];
+};
+
+function main() {
+  let stones = input;
+
+  for (let i = 0; i < 25; i++) {
+    stones = stones.flatMap(blinkAtStone);
+  }
+
+  return stones.length;
+}
+
+export default main();
--- src/2024/day11/part2.ts
+++ src/2024/day11/part2.ts
@@ -1,5 +1,51 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
+import { it } from "node:test";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: " ",
+  },
+});
 
-// TODO: Complete Part 2
+const cache = new Map<string, number>();
+
+const blinkAtStone = (number: number): number[] => {
+  if (number === 0) {
+    return [1];
+  }
+
+  const numbers = `${number}`;
+  if (numbers.length % 2 === 0) {
+    const mid = Math.ceil(numbers.length / 2);
+    return [Number(numbers.slice(0, mid)), Number(numbers.slice(mid))];
+  }
+
+  return [number * 2024];
+};
+
+const recurseBlinks = (number: number, iteration: number): number => {
+  const cacheKey = `${number}_${iteration}`;
+  if (cache.has(cacheKey)) {
+    return cache.get(cacheKey)!;
+  }
+
+  if (iteration <= 0) {
+    return 1;
+  }
+
+  const stones = blinkAtStone(number);
+
+  const count = stones.reduce((acc, stone) => {
+    return acc + recurseBlinks(stone, iteration - 1);
+  }, 0);
+
+  cache.set(cacheKey, count);
+
+  return count;
+};
+
+function main() {
+  return input.reduce((acc, stone) => acc + recurseBlinks(stone, 75), 0);
+}
+
+export default main();
