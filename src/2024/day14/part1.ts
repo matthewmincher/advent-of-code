00783dc72e61d540f401d18a33f01819a2623a04
Matthew Mincher <matthew@mincher.org>
2024-12-23 15:47:46 +0000

    2024.14

--- src/2024/day14/part1.ts
+++ src/2024/day14/part1.ts
@@ -1,5 +1,91 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
+import { isNumberObject } from "node:util/types";
 
-const input = parseInput();
+const input = parseInput<Robot>({
+  split: {
+    delimiter: "\n",
+    mapper: (line) => {
+      const [x, y, vx, vy] = line.match(/-?\d+/g)!.map((s) => Number(s));
 
-// TODO: Complete Part 1
+      return {
+        position: {
+          x,
+          y,
+        },
+        velocity: {
+          x: vx,
+          y: vy,
+        },
+      };
+    },
+  },
+});
+
+const WIDTH = 101;
+const HEIGHT = 103;
+
+type Robot = {
+  position: Position;
+  velocity: {
+    x: number;
+    y: number;
+  };
+};
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+const move = (robot: Robot): Robot => {
+  const newX =
+    (((robot.position.x + robot.velocity.x) % WIDTH) + WIDTH) % WIDTH;
+  const newY =
+    (((robot.position.y + robot.velocity.y) % HEIGHT) + HEIGHT) % HEIGHT;
+
+  return {
+    position: {
+      x: newX,
+      y: newY,
+    },
+    velocity: robot.velocity,
+  };
+};
+
+function main() {
+  for (let i = 0; i < 100; i++) {
+    for (const key in input) {
+      input[key] = move(input[key]);
+    }
+  }
+
+  const quadrants = input.reduce(
+    (acc, robot) => {
+      const w = WIDTH - 1;
+      const h = HEIGHT - 1;
+      const x = robot.position.x / w;
+      const y = robot.position.y / h;
+
+      if (robot.position.x === w / 2 || robot.position.y === h / 2) {
+        return acc;
+      }
+      acc[`${Math.round(x)}_${Math.round(y)}` as keyof typeof acc]++;
+
+      return acc;
+    },
+    {
+      "0_0": 0,
+      "0_1": 0,
+      "1_0": 0,
+      "1_1": 0,
+    },
+  );
+
+  console.log(quadrants);
+
+  return (
+    quadrants["0_0"] * quadrants["0_1"] * quadrants["1_0"] * quadrants["1_1"]
+  );
+}
+
+export default main();
--- src/2024/day14/part2.ts
+++ src/2024/day14/part2.ts
@@ -1,5 +1,83 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
+import { isNumberObject } from "node:util/types";
 
-const input = parseInput();
+const input = parseInput<Robot>({
+  split: {
+    delimiter: "\n",
+    mapper: (line) => {
+      const [x, y, vx, vy] = line.match(/-?\d+/g)!.map((s) => Number(s));
 
-// TODO: Complete Part 2
+      return {
+        position: {
+          x,
+          y,
+        },
+        velocity: {
+          x: vx,
+          y: vy,
+        },
+      };
+    },
+  },
+});
+
+const WIDTH = 101;
+const HEIGHT = 103;
+
+type Robot = {
+  position: Position;
+  velocity: {
+    x: number;
+    y: number;
+  };
+};
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+const move = (robot: Robot): Robot => {
+  const newX =
+    (((robot.position.x + robot.velocity.x) % WIDTH) + WIDTH) % WIDTH;
+  const newY =
+    (((robot.position.y + robot.velocity.y) % HEIGHT) + HEIGHT) % HEIGHT;
+
+  return {
+    position: {
+      x: newX,
+      y: newY,
+    },
+    velocity: robot.velocity,
+  };
+};
+
+const print = (i: number, robots: Robot[]) => {
+  console.log(i);
+  const map: number[][] = JSON.parse(
+    JSON.stringify(new Array(HEIGHT).fill(new Array(WIDTH).fill(0))),
+  );
+
+  for (const robot of robots) {
+    map[robot.position.y][robot.position.x]++;
+  }
+
+  for (const row of map) {
+    console.log(row.map((n) => (n > 0 ? "R" : ".")).join(""));
+  }
+};
+
+function main() {
+  for (let i = 0; i < 10000; i++) {
+    for (const key in input) {
+      input[key] = move(input[key]);
+    }
+
+    print(i, input);
+  }
+
+  // Yeah... I just searched the output in phpstorm
+  return 0;
+}
+
+export default main();
