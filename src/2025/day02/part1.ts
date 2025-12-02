48ba6ea9e21eb50e804e47bbee6774647cb3c9fa
Matthew Mincher <matthew@mincher.org>
2025-12-02 22:18:42 +0000

    2025.2

--- src/2025/day02/part1.ts
+++ src/2025/day02/part1.ts
@@ -1,5 +1,25 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) =>
+      line.split(",").map((range) => range.split("-").map(Number)),
+  },
+})[0];
 
-// TODO: Complete Part 1
+function main() {
+  const invalidIds: Array<number> = [];
+  const pattern = /^(\d+)\1$/;
+
+  for (const [min, max] of input) {
+    for (let i = min; i <= max; i++) {
+      if (pattern.test(`${i}`)) {
+        invalidIds.push(i);
+      }
+    }
+  }
+
+  return invalidIds.reduce((sum, id) => sum + id, 0);
+}
+
+export default main();
