2709228170cf1651fd33680b2372d1f29874871e
Matthew Mincher <matthew@mincher.org>
2025-12-03 19:25:15 +0000

    Day 3

--- src/2025/day03/part1.ts
+++ src/2025/day03/part1.ts
@@ -1,5 +1,43 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) => line.split("").map(Number),
+  },
+});
 
-// TODO: Complete Part 1
+function main() {
+  let totalJoltage = 0;
+
+  for (const numbers of input) {
+    if (numbers.length === 0) {
+      //ty phpstorm very cool
+      continue;
+    }
+
+    let first = -1;
+    let firstIndex = -1;
+    let second = -1;
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i] > first) {
+        first = numbers[i];
+        firstIndex = i;
+      }
+    }
+
+    for (let i = firstIndex + 1; i < numbers.length; i++) {
+      if (numbers[i] > second) {
+        second = numbers[i];
+      }
+    }
+
+    const number = Number(`${first}${second}`);
+
+    totalJoltage += number;
+  }
+
+  return totalJoltage;
+}
+
+export default main();
--- src/2025/day03/part2.ts
+++ src/2025/day03/part2.ts
@@ -1,5 +1,39 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) => line.split("").map(Number),
+  },
+});
 
-// TODO: Complete Part 2
+function main() {
+  let totalJoltage = 0;
+
+  for (const numbers of input) {
+    if (numbers.length === 0) {
+      //ty phpstorm very cool
+      continue;
+    }
+
+    let selection: Array<number> = [];
+
+    for (let digitsRequired = 12, i = 0; digitsRequired > 0; digitsRequired--) {
+      const possibleDigits = numbers.slice(
+        i,
+        numbers.length - digitsRequired + 1,
+      );
+      const max = Math.max(...possibleDigits);
+
+      i += possibleDigits.indexOf(max) + 1;
+      selection.push(max);
+    }
+
+    const number = Number(selection.join(""));
+
+    totalJoltage += number;
+  }
+
+  return totalJoltage;
+}
+
+export default main();
