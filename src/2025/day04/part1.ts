4bc31580a1fe1421aae0ce62d0df8ededa9dd215
Matthew Mincher <matthew@mincher.org>
2025-12-04 16:52:57 +0000

    2025.4

--- src/2025/day04/part1.ts
+++ src/2025/day04/part1.ts
@@ -1,5 +1,43 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) => line.split(""),
+  },
+});
 
-// TODO: Complete Part 1
+function main() {
+  let accessible = 0;
+
+  for (let y = 0; y < input.length; y++) {
+    const row = input[y];
+
+    for (let x = 0; x < row.length; x++) {
+      if (row[x] !== "@") {
+        continue;
+      }
+
+      let adjacent = 0;
+
+      for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+          if (dx === 0 && dy === 0) {
+            continue;
+          }
+
+          if (input[y + dy]?.[x + dx] === "@") {
+            adjacent++;
+          }
+        }
+      }
+
+      if (adjacent < 4) {
+        accessible++;
+      }
+    }
+  }
+
+  return accessible;
+}
+
+export default main();
--- src/2025/day04/part2.ts
+++ src/2025/day04/part2.ts
@@ -1,5 +1,47 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) => line.split(""),
+  },
+});
 
-// TODO: Complete Part 2
+function main() {
+  let accessible = 0;
+
+  let removed;
+  do {
+    removed = 0;
+    for (let y = 0; y < input.length; y++) {
+      for (let x = 0; x < input[y].length; x++) {
+        if (input[y][x] !== "@") {
+          continue;
+        }
+
+        let adjacent = 0;
+
+        for (let dx = -1; dx <= 1; dx++) {
+          for (let dy = -1; dy <= 1; dy++) {
+            if (dx === 0 && dy === 0) {
+              continue;
+            }
+
+            if (input[y + dy]?.[x + dx] === "@") {
+              adjacent++;
+            }
+          }
+        }
+
+        if (adjacent < 4) {
+          input[y][x] = ".";
+          accessible++;
+          removed++;
+        }
+      }
+    }
+  } while (removed > 0);
+
+  return accessible;
+}
+
+export default main();
