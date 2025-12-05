b06848f1f36d8c4dee4e5c2ca11e0e8f45cf4101
Matthew Mincher <matthew@mincher.org>
2025-12-05 08:00:18 +0000

    2025.5

--- src/2025/day05/part1.ts
+++ src/2025/day05/part1.ts
@@ -1,5 +1,21 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: "\n\n",
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 1
+function main() {
+  const ranges = input[0].split("\n").map((r) => r.split("-").map(Number));
+  const items = input[1].split("\n").map(Number);
+
+  const fresh = items.filter((num) =>
+    ranges.some((r) => num >= r[0] && num <= r[1]),
+  );
+
+  return fresh.length;
+}
+
+export default main();
--- src/2025/day05/part2.ts
+++ src/2025/day05/part2.ts
@@ -1,5 +1,32 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    delimiter: "\n\n",
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+function main() {
+  let ranges = input[0]
+    .split("\n")
+    .map((r) => r.split("-").map(Number))
+    .sort((a, b) => a[0] - b[0]);
+
+  let i = 1;
+  while (i < ranges.length) {
+    let r = ranges[i];
+    let prev = ranges[i - 1];
+
+    if (r[0] >= prev[0] && r[0] <= prev[1]) {
+      ranges[i - 1] = [prev[0], Math.max(prev[1], r[1])];
+      ranges.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return ranges.reduce((acc, range) => acc + (range[1] - range[0]) + 1, 0);
+}
+
+export default main();
