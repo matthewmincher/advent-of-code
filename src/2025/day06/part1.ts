db3c3d2f2c8b4b62e462dbfb24c50aeb83b579b2
Matthew Mincher <matthew@mincher.org>
2025-12-09 22:58:47 +0000

    2025.6 so help me god phpstorm leave my whitespace alone

--- src/2025/day06/part1.ts
+++ src/2025/day06/part1.ts
@@ -1,5 +1,34 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: (line) => {
+      return line.split(" ").filter(Boolean);
+    },
+  },
+});
 
-// TODO: Complete Part 1
+function main() {
+  let runningTotal = 0;
+
+  for (let i = 0; i < input[0].length; i++) {
+    const op = input[4][i];
+    const els = [input[0][i], input[1][i], input[2][i], input[3][i]];
+
+    const result = els.reduce((sum, el) => {
+      const num = Number(el);
+
+      if (op === "+") {
+        return sum + num;
+      } else {
+        return Math.max(sum, 1) * num;
+      }
+    }, 0);
+
+    runningTotal += result;
+  }
+
+  return runningTotal;
+}
+
+export default main();
--- src/2025/day06/part2.ts
+++ src/2025/day06/part2.ts
@@ -1,5 +1,45 @@
-import { parseInput } from '../../util';
+import { parseInput } from "../../util";
 
-const input = parseInput();
+const input = parseInput({
+  split: {
+    mapper: false,
+  },
+});
 
-// TODO: Complete Part 2
+function main() {
+  let runningTotal = 0;
+
+  let number: string[] = [];
+
+  for (let col = input[0].length - 1; col >= 0; col--) {
+    let char = "";
+    for (let row = 0; row < input.length - 1; row++) {
+      let c = input[row].charAt(col);
+
+      char += c;
+    }
+    number.push(char);
+
+    const op = input[input.length - 1].charAt(col).trim();
+    if (!op) {
+      continue;
+    }
+
+    runningTotal += number.reduce((sum, el) => {
+      const num = Number(el);
+
+      if (op === "+") {
+        return sum + num;
+      } else {
+        return Math.max(sum, 1) * num;
+      }
+    }, 0);
+
+    number = [];
+    col--;
+  }
+
+  return runningTotal;
+}
+
+export default main();
